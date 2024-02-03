85b33a241106c22d6e91a4a582c0f7df5244d3a1
Ekarika Nsemeke <hamatthan@gmail.com>
2024-02-03 17:39:02 +0100

    Confirmed the contract on Etherscan

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
-const { URL, PRIVATE_KEY } = process.env;
+const { URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 module.exports = {
   solidity: "0.8.19",
   networks: {
@@ -10,5 +10,8 @@ module.exports = {
       url: URL,
       accounts: [`0x${PRIVATE_KEY}`],
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
